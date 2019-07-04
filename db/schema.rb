98746368ee584b74503712790682ddc0b61b44dc
elijahmm17 <elijahmm17@gmail.com>
2019-07-04 03:29:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema.define(version: 1) do
   create_table "students", force: :cascade do |t|
-    
+    t.string :name 
+    t.integer :grade
+    t.string :birthdate
   end
 end
