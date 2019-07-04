504297a6f7bb219bda4020f0e6e41d98f801693c
elijahmm17 <elijahmm17@gmail.com>
2019-07-04 03:26:48 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  crate_table :students do |t|
+  create_table :students do |t|
     t.string :name
     t.integer :grade
     t.string :birthdate
